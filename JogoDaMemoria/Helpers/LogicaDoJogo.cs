ec87f3a2440ce98d4b92d4d520f8c42e0557862b
Rosemberg Torres <rosemberg@opovodigital.com>
2019-02-15 16:30:45 -0300

    customizando layout

--- JogoDaMemoria/Helpers/LogicaDoJogo.cs
+++ JogoDaMemoria/Helpers/LogicaDoJogo.cs
@@ -12,42 +12,38 @@ namespace JogoDaMemoria.Helpers {
 
         public async Task Logica(Button BtnFrente, Button BtnAtras, int ContadorDeJogadas, Dictionary<string, List<Button>> Jogadas) {
 
-            if (Jogadas.Count.Equals(5)) {
-                MessagingCenter.Send("Fim", "UsuarioEncontrouTodas");
-            } else {
-                RotacionarBotao Rotacionar = new RotacionarBotao();
+            RotacionarBotao Rotacionar = new RotacionarBotao();
 
-                await Rotacionar.RotacionarBtnParaAtras(BtnFrente, BtnAtras);
+            await Rotacionar.RotacionarBtnParaAtras(BtnFrente, BtnAtras);
 
-                if (!Jogadas.ContainsKey("jogada" + ContadorDeJogadas)) { // SE FOR A PRIMEIRA JOGADA
+            if (!Jogadas.ContainsKey("jogada" + ContadorDeJogadas)) { // SE FOR A PRIMEIRA JOGADA
 
-                    List<Button> Buttons = new List<Button> { BtnFrente, BtnAtras };
-                    Jogadas.Add("jogada" + ContadorDeJogadas, Buttons);
+                List<Button> Buttons = new List<Button> { BtnFrente, BtnAtras };
+                Jogadas.Add("jogada" + ContadorDeJogadas, Buttons);
 
-                } else { // SE FOR A SEGUNDA JOGADA
+            } else { // SE FOR A SEGUNDA JOGADA
 
-                    var UltimaJogadaDoUsuario = Jogadas["jogada" + ContadorDeJogadas];
-                    var BtnFrenteDaUltimaJogadaDoUsuario = Jogadas["jogada" + ContadorDeJogadas][0];
-                    var BtnAtrasDaUltimaJogadaDoUsuario = Jogadas["jogada" + ContadorDeJogadas][1];
+                var UltimaJogadaDoUsuario = Jogadas["jogada" + ContadorDeJogadas];
+                var BtnFrenteDaUltimaJogadaDoUsuario = Jogadas["jogada" + ContadorDeJogadas][0];
+                var BtnAtrasDaUltimaJogadaDoUsuario = Jogadas["jogada" + ContadorDeJogadas][1];
 
-                    var CorDaUltimaJogada = BtnAtrasDaUltimaJogadaDoUsuario.BackgroundColor;
+                var CorDaUltimaJogada = BtnAtrasDaUltimaJogadaDoUsuario.BackgroundColor;
 
-                    if (CorDaUltimaJogada.Equals(BtnAtras.BackgroundColor)) { // QUANDO ACERTAR
+                if (CorDaUltimaJogada.Equals(BtnAtras.BackgroundColor)) { // QUANDO ACERTAR
 
-                        UltimaJogadaDoUsuario.Add(BtnFrente);
-                        UltimaJogadaDoUsuario.Add(BtnAtras);
+                    UltimaJogadaDoUsuario.Add(BtnFrente);
+                    UltimaJogadaDoUsuario.Add(BtnAtras);
 
-                        MessagingCenter.Send("Incrementar", "IncrementarContadorDeJogadas");
+                    MessagingCenter.Send("Incrementar", "IncrementarContadorDeJogadas");
 
-                    } else { // QUANDO ERRAR
+                } else { // QUANDO ERRAR
 
-                        // Desfaz a rotação dos botões selecionados
-                        await Rotacionar.RotacionarBtnParaFrente(BtnFrente, BtnAtras);
-                        await Rotacionar.RotacionarBtnParaFrente(BtnFrenteDaUltimaJogadaDoUsuario, BtnAtrasDaUltimaJogadaDoUsuario);
+                    // Desfaz a rotação dos botões selecionados
+                    await Rotacionar.RotacionarBtnParaFrente(BtnFrente, BtnAtras);
+                    await Rotacionar.RotacionarBtnParaFrente(BtnFrenteDaUltimaJogadaDoUsuario, BtnAtrasDaUltimaJogadaDoUsuario);
 
-                        // Zera a jogada
-                        Jogadas.Remove("jogada" + ContadorDeJogadas);
-                    }
+                    // Zera a jogada
+                    Jogadas.Remove("jogada" + ContadorDeJogadas);
                 }
             }
         }
